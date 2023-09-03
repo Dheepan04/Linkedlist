48af4039e055168503d7e4a2b0892deebdc820f9
Dheepan04 <dheepangrasp@gmail.com>
2023-09-04 01:32:32 +0530

    DSA-LinkedList

---
+++ LL-countnodesandsums.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void create(int A[],int n){
+    int i;
+    struct Node *t,*last;
+    first=new Node[sizeof(struct Node)];
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++){
+        t=new Node[sizeof(struct Node)];
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+int count(struct Node *p){
+    int l=0;
+    while(p!=NULL){
+        l++;
+        p=p->next;
+    }
+    return l;
+}
+int sum(struct Node *p){
+    int s=0;
+    while(p!=NULL){
+        s+=p->data;
+        p=p->next;
+    }
+    return s;
+}
+
+int main(){
+    int A[]={3,4,5,6,8};
+    create(A,5);
+    cout<<"Count = "<<count(first)<<endl;
+    cout<<"Sum = "<<sum(first);
+}
